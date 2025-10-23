d55a68267c3a3d8e618e946ecb4e78e28aeefff5
Alex <alexandrzenchenko1@gmail.com>
2025-10-23 13:31:30 +0300

    added icons for navigation button

--- app/modules/common/navigation/AppStack.tsx
+++ app/modules/common/navigation/AppStack.tsx
@@ -1,12 +1,18 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ChatsScreen from "@/app/modules/chat/screens/Chats";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function AppStack() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Chats" component={ChatsScreen} />
+      <Tab.Screen name="Chats" component={ChatsScreen} options={{
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="chat" color={color} size={size} />
+        ),
+      }}
+      />
     </Tab.Navigator>
 );
 }
