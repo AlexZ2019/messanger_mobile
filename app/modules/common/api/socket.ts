30dd52c0730039540791bd5c8acfbe3009f837f9
Alex <alexandrzenchenko1@gmail.com>
2025-11-11 16:27:15 +0200

    added socket connection

---
+++ app/modules/common/api/socket.ts
@@ -0,0 +1,19 @@
+import { io, Socket } from 'socket.io-client';
+
+const SOCKET_URL = 'http://192.168.123.33:3001';
+let socket: Socket;
+
+export const getSocket = () => {
+  if (!socket) {
+    socket = io(SOCKET_URL, {
+      path: '/chat',
+      transports: ['websocket'],
+      autoConnect: false,
+    });
+
+    socket.on('connect', () => console.log('✅ Socket connected:', socket.id));
+    socket.on('disconnect', (reason) => console.log('❌ Socket disconnected:', reason));
+    socket.on('connect_error', (err) => console.error('❌ Connection error:', err));
+  }
+  return socket;
+};
