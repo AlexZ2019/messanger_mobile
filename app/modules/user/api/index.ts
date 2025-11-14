4102de2703fb8f4b7e6e43172de65c1ca4e81ae4
Alex <alexandrzenchenko1@gmail.com>
2025-11-14 14:16:26 +0200

    clean up

--- app/modules/user/api/index.ts
+++ app/modules/user/api/index.ts
@@ -1,5 +1,4 @@
 import {axiosClient} from "@/app/modules/common/api";
-import {LocalContact} from "@/app/modules/user/types";
 
 export const userApi = {
   getUser: async (): Promise<{ id: number; email: string; name?: string } | null> => {
