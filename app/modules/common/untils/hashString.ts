7976dc033e3a9ecd26adc379aa75fa5f23aad960
Alex <alexandrzenchenko1@gmail.com>
2025-11-19 13:19:05 +0200

    replaced to arrow function

--- app/modules/common/untils/hashString.ts
+++ app/modules/common/untils/hashString.ts
@@ -1,9 +1,10 @@
 import * as Crypto from 'expo-crypto';
 
-export async function sha256(str?: string): Promise<string | null> {
-  if (!str) return null
+export const sha256 = async (str?: string): Promise<string | null> => {
+  if (!str) return null;
+
   return Crypto.digestStringAsync(
     Crypto.CryptoDigestAlgorithm.SHA256,
     str
   );
-}
+};
